e2088b8994630d5284868718e701132544b71b9b
dpa722 <daniel.dpa722@gmail.com>
2019-04-11 19:49:15 -0400

    Update sprint1.sql

--- sprint1/sprint1.sql
+++ sprint1/sprint1.sql
@@ -1,20 +1,19 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.1.30-MariaDB - mariadb.org binary distribution
+-- Versión del servidor:         5.5.5-10.1.30-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win32
--- HeidiSQL Versión:             10.1.0.5464
+-- HeidiSQL Versión:             8.3.0.4694
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 
 -- Volcando estructura de base de datos para sprint1
 CREATE DATABASE IF NOT EXISTS `sprint1` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `sprint1`;
+
 
 -- Volcando estructura para tabla sprint1.documentos
 CREATE TABLE IF NOT EXISTS `documentos` (
@@ -24,7 +23,24 @@ CREATE TABLE IF NOT EXISTS `documentos` (
   PRIMARY KEY (`coddocumento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sprint1.documentos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `documentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `documentos` ENABLE KEYS */;
+
+
+-- Volcando estructura para tabla sprint1.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `nroingreso` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(50) NOT NULL,
+  PRIMARY KEY (`nroingreso`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla sprint1.login: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` (`nroingreso`, `usuario`, `contraseña`) VALUES
+	(1, 'daniel', 'daniel');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
